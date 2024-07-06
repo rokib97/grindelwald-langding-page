c1b58ebad0a93af20baee584a6233a7539d09650
rokib97 <hasanrokib1997@gmail.com>
2024-07-07 04:10:00 +0600

    refactor some code , added new fonts for heading

--- js/main.js
+++ js/main.js
@@ -9,12 +9,16 @@ document.addEventListener("DOMContentLoaded", () => {
   const button = document.getElementById("next");
   const imageElements = document.querySelectorAll(".slider .slide img");
 
-  button.addEventListener("click", () => {
+  const changeSlide = () => {
+    console.log("object");
     currentImageIndex = (currentImageIndex + 1) % images.length;
     for (let i = 0; i < imageElements.length; i++) {
       imageElements[i].src = images[(currentImageIndex + i) % images.length];
     }
-  });
-});
+  };
 
-// second slider js implementation
+  button.addEventListener("click", changeSlide);
+
+  // Set interval to change slide every 5 seconds
+  setInterval(changeSlide, 5000);
+});
