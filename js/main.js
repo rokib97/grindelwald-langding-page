42aa8bbc3f80c9d9b5ae930e9704ebc061ef6d1c
rokib97 <hasanrokib1997@gmail.com>
2024-07-07 08:46:10 +0600

    done teh scripting for retreat section slider

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// first slider js implementation
+// first slider script here
 document.addEventListener("DOMContentLoaded", () => {
   const images = [
     "./assets/images/slide1.png",
@@ -21,4 +21,28 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Set interval to change slide every 5 seconds
   setInterval(changeSlide, 5000);
+});
+
+// second slider script here
+const slider = document.querySelector(".unique-slider");
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+prevBtn.addEventListener("click", () => {
+  slider.style.transition = "none";
+  slider.insertBefore(slider.lastElementChild, slider.firstElementChild);
+  slider.style.transform = "translateX(-378px)";
+  requestAnimationFrame(() => {
+    slider.style.transition = "transform 0.5s ease-in-out";
+    slider.style.transform = "translateX(0)";
+  });
+});
+
+nextBtn.addEventListener("click", () => {
+  slider.style.transition = "none";
+  slider.appendChild(slider.firstElementChild);
+  slider.style.transform = "translateX(378px)";
+  requestAnimationFrame(() => {
+    slider.style.transition = "transform 0.5s ease-in-out";
+    slider.style.transform = "translateX(0)";
+  });
 });
